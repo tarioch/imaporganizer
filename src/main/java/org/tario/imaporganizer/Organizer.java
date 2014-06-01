3371b45f271bfa52c93ed53f58836b9dbcaf8acc
Patrick Ruckstuhl <patrick@ch.tario.org>
2014-06-01 12:12:12 +0200

    Working connection to imap with externalized configuration

--- src/main/java/org/tario/imaporganizer/Organizer.java
+++ src/main/java/org/tario/imaporganizer/Organizer.java
@@ -1,13 +1,56 @@
 package org.tario.imaporganizer;
 
+import java.util.Properties;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Store;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Organizer {
+	final String server;
+	final String user;
+	final String password;
+	final boolean ssl;
+	final String port;
 
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	@Autowired
+	public Organizer(@Value("${conf.server}") String server, @Value("${conf.user}") String user,
+			@Value("${conf.password}") String password, @Value("${conf.ssl}") boolean ssl,
+			@Value("${conf.port}") String port) {
+		this.server = server;
+		this.user = user;
+		this.password = password;
+		this.ssl = ssl;
+		this.port = port;
+	}
+
+	public void run() throws Exception {
+
+		final String protocol = ssl ? "imaps" : "imap";
+		final Properties props = new Properties();
+		props.setProperty("mail.store.protocol", protocol);
+		props.setProperty("mail." + protocol + ".host", server);
+		props.setProperty("mail." + protocol + ".port", port);
+
+		final Session session = Session.getDefaultInstance(props);
+		final Store store = session.getStore(protocol);
+		store.connect(server, user, password);
+		System.out.println(store.isConnected());
+
+		final Folder folder = store.getFolder("INBOX");
+		folder.open(Folder.READ_ONLY);
+		final Message[] messages = folder.getMessages();
+		for (final Message message : messages) {
+			System.out.println(message.getSubject());
+		}
+
+		store.close();
 	}
 
 }
--- src/main/java/org/tario/imaporganizer/OrganizerApplication.java
+++ src/main/java/org/tario/imaporganizer/OrganizerApplication.java
@@ -2,14 +2,23 @@ package org.tario.imaporganizer;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan
+@PropertySource("file:imaporganizer.properties")
 public class OrganizerApplication {
 
-	public static void main(String[] args) {
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
+
+	public static void main(String[] args) throws Exception {
 		try (ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(OrganizerApplication.class)) {
 			context.getBean(Organizer.class).run();
 		}
